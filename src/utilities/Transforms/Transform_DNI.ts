fc30752b62fdbdc33d20a33075c09bc2d7c7f508
Miguel <miguelangel5cebrian@gmail.com>
2026-05-18 16:20:54 +0200

    ft. Creación de nuevas utilities

---
+++ src/utilities/Transforms/Transform_DNI.ts
@@ -0,0 +1,24 @@
+import CryptoJS from "crypto-js";
+
+export const Encrypt_DNI = (dni: string) => {
+    const DNI_KEY = import.meta.env.VITE_DNI_KEY;
+    
+    if(!DNI_KEY){
+        return undefined;
+    }
+
+    return CryptoJS.AES.encrypt(dni.trim(), DNI_KEY).toString();
+}
+
+export const Decrypt_DNI = (dni: string) => {
+    const DNI_KEY = import.meta.env.VITE_DNI_KEY;
+    
+    if(!DNI_KEY){
+        return undefined;
+    }
+
+    const bytes = CryptoJS.AES.decrypt(dni.trim(), DNI_KEY);
+    console.log("Decode: "+ bytes.toString(CryptoJS.enc.Utf8));
+
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
---
+++ src/utilities/Transforms/Transform_Passwords.ts
@@ -0,0 +1,24 @@
+import CryptoJS from "crypto-js";
+
+export const Encrypt_Passwords = (password: string) => {
+    const DNI_KEY = import.meta.env.VITE_DNI_KEY;
+    
+    if(!DNI_KEY){
+        return undefined;
+    }
+
+    return CryptoJS.AES.encrypt(password.trim(), DNI_KEY).toString();
+}
+
+export const Decrypt_Passwords = (password: string) => {
+    const DNI_KEY = import.meta.env.VITE_DNI_KEY;
+    
+    if(!DNI_KEY){
+        return undefined;
+    }
+
+    const bytes = CryptoJS.AES.decrypt(password.trim(), DNI_KEY);
+    console.log("Decode: "+ bytes.toString(CryptoJS.enc.Utf8));
+
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
