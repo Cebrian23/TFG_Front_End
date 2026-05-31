96eeb391a18be10754745d23dbc850600ebb3c79
Miguel <miguelangel5cebrian@gmail.com>
2026-05-31 10:58:08 +0200

    ft. Creación de un header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+function Header() {
+    return(
+        <div className="headers">
+            <h3>Gestor de master interuniversitario</h3>
+        </div>
+    )
+}
+
+export default Header;
