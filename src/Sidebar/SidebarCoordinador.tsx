46bfb64b970e401c6036785db1c94f0806eb9b36
Miguel <miguelangel5cebrian@gmail.com>
2026-06-30 00:25:38 +0200

    ft. Actualización de los botones del sidebar

--- src/Sidebar/SidebarCoordinador.tsx
+++ src/Sidebar/SidebarCoordinador.tsx
@@ -22,7 +22,7 @@ function SidebarCoordinador() {
             <h4>Gestionar asignaturas impartidas</h4>
             <button type="button" onClick={() => globalThis.location.href = "/mostrarAsignaturas"}>Ver asignaturas impartidas</button>
             <h4>Gestionar TFM de alumnos</h4>
-            <button type="button" onClick={() => globalThis.location.href = "/nuevoTFM"}>Insertar TFM de un alumno</button>
+            <button type="button" onClick={() => globalThis.location.href = "/nuevoTFM"}>Calificar TFM de un alumno</button>
             <h4>Gestionar información de las calificaciones de los estudiantes</h4>
             <button type="button" onClick={() => globalThis.location.href = "/controlCalidad"}>Ver métricas de calidad</button>
             <br/>
